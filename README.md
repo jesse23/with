a8efa43d6462b2b8565ea07b1fed93e0fe329a09
jesse23 <pengwenjia@gmail.com>
2017-11-24 00:20:08 -0500

    do refactor based on testing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # with for fzf
 A helper batch for [fzf](https://github.com/junegunn/fzf) on Windows.
 
-![Preview](./preview.gif?raw=true "Preview")
+![Preview](./doc/preview.gif?raw=true "Preview")
 
 
 On Linux, the common practice for `fzf` is:
@@ -22,6 +22,10 @@ On Windows platform, we can use pip to achieve same approach.
 1. by `with <app>`, it will call file search function provided by `fzf` and pick the last selection and pass to `app` as parameter.
 2. by `wtih` only, it will call `start <param>` which will do default open to the selection.
 3. by `<command> | with <app>` you get the complete filter and execute function.
+
+## Test
+
+<iframe src="./doc/with_test.htm" frameborder="0" scrolling="no"></iframe>
 
 ## Reference
 
--- with.bat
+++ with.bat
@@ -1,9 +1,54 @@
 @echo off
+setlocal
+
+:: Do FZF Setting Here
+:: set FZF_DEFAULT_COMMAND=fd -a -j 4
+:: set FZF_DEFAULT_OPTS=--prompt="=>" --border --inline-info 
+
+:: Process executable input
+set EXECUTABLE=%1
+
+:::: If just 'with', no executable so no ext
+if not defined EXECUTABLE goto EXEFZF
+
+:::: Get extension and file name
+for /f "usebackq delims=" %%a in (`where %EXECUTABLE%`) do (set EXE_EXT=%%~xa) && (set EXE_NAME=%%~na)
+
+:::: NOTE: for support *.cmd, we can use example below
+:::: set "formats=.cmd.bat."
+:::: for /f "usebackq delims=" %%a in (`where %EXECUTABLE%`) do (
+::::   if "!formats:%%~xa.=!" neq "!formats!" (
+::::     set IS_BAT=true
+::::   ) 
+::::   set EXE_EXT=%%~xa
+::::   set EXE_NAME=%%~na
+:::: )
+
+:::: If still no ext here, means EXECUTABLE is invalid
+if not defined EXE_EXT goto END
+
+:EXEFZF
+:: Run fzf to get selection
 for /f "usebackq delims=" %%i in (`fzf`) do set SELECTION=%%~fi
-if not defined ConEmuHWND goto EXECMD
-:: for skip 10 sec warning in conEmu
-start "" %1 %SELECTION% -cur_console:n
-goto END
-:EXECMD
-start "" %1 %SELECTION% 
+
+:::: Go to end if no selection
+if not defined SELECTION goto END 
+
+:: Do execution
+:::: If not BAT go to WITH_START
+if not "%EXE_EXT%" == ".bat" goto WITH_START
+
+:::: Hard code for vim.bat case
+if "%EXE_NAME%" == "vim" goto WITH_START
+
+:WITH_CALL
+call %1 "%SELECTION%"
+goto :END
+
+:WITH_START
+start "winpty" %1 "%SELECTION%" -cur_console:n
+goto :END
+
 :END
+
+
